637f4cb9b71f8f4785d5ccf7d50c5432f9d7c94e
santamadaraneilande-ctrl <santamadaraneilande@gmail.com>
2026-08-24 20:14:12 +0300

    Izlabota viewport drukas kļūda

--- generate.py
+++ generate.py
@@ -52,7 +52,7 @@ def render_html(data):
 <html lang="lv">
 <head>
 <meta charset="UTF-8">
-<meta name="veiwport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Portfolio - {name}</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
